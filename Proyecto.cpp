43e5090356c3f16f346e77ede9de8848ec6b74c9
HDZHDZ2 <31052396+HDZHDZ2@users.noreply.github.com>
2017-11-21 20:27:33 -0600

    Add files via upload

---
+++ Proyecto.cpp
@@ -0,0 +1,399 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int op;
+main()
+{
+	do
+	{
+		puts("\t\t\tCIENCIAS MEDICAS");
+		puts("1) GINECOLOGIA ");
+		puts("2) PEDIATRIA ");
+		puts("3) FISIOTERAPIA ");
+		puts("4) SALIR ");
+		puts("ESCOGE UNA AREA");
+		scanf("%d",&op);	
+		system("cls");
+	switch(op)
+	{
+	
+			case 1:
+			
+			do 
+			{
+			
+			puts("\t\t\tBIENVENIDO A GINECOLOGIA");
+			puts("DOCTORES EN ZONA");
+			puts("1) DRA. LAURA MARIA SANCHEZ IBARRA");
+			puts("2) DR. ALEJANDRO CALDERON RAMIREZ");
+			puts("3) DR. MAURICIO CERVANTES HERNANDEZ");
+			puts("4) SALIR");
+			puts("ESCOGE UNA OPCION");
+			scanf("%d",&op);
+			system("cls");
+					switch(op)
+					{
+						case 1:
+							
+							puts("\t\t BIENVENIDO, ESCOGIO A DRA. LAURA MARIA SANCHEZ IBARRA");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+						
+						
+						case 2:
+						
+							
+						
+							
+							puts("\t\t BIENVENIDO, ESCOGIO A DR. ALEJANDRO CALDERON RAMIREZ");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+							
+						
+						case 3:
+							
+							
+						
+							
+							puts("\t\t BIENVENIDO, ESCOGIO A DR. MAURICIO CERVANTES HERNANDEZ");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+							
+						
+							
+					}
+			
+			}while(op!=4);	
+			break;
+			
+	
+			case 2:
+				
+				
+			do 
+			{
+			
+			puts("\t\t\tBIENVENIDO A PEDIATRIA");
+			puts("DOCTORES EN ZONA");
+			puts("1) DR. FERNANDO JOSE TORRES SANZ");
+			puts("2) DRA. LAURA ROCUZZO BAEZ");
+			puts("3) DRA. CLAUDIA ITZEL HERNANDNEZ RUIZ");
+			puts("4) SALIR");
+			puts("ESCOGE UNA OPCION");
+			scanf("%d",&op);
+			system("cls");
+					switch(op)
+					{
+						case 1:
+							
+							puts("\t\t BIENVENIDO, ESCOGIO A DR. FERNANDO JOSE TORRES SANZ");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+							
+						
+						case 2:
+						
+							
+							
+							puts("\t\t BIENVENIDO, ESCOGIO A DRA. LAURA ROCUZZO BAEZ");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+							
+						
+						case 3:
+							
+						
+							
+							puts("\t\t BIENVENIDO, ESCOGIO A DRA. CLAUDIA ITZEL HERNANDNEZ RUIZ");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+							
+						
+							
+					}
+			
+			}while(op!=4);	
+			break;
+			
+			
+		break;
+		
+		case 3:
+			
+			
+			do 
+			{
+			
+			puts("\t\t\tBIENVENIDO A FISIOTERAPIA");
+			puts("DOCTORES EN ZONA");
+			puts("1) DR. ANDRES INIESTA LUJAN");
+			puts("2) DR. JOSE DE JESUS CORONA RODRIGUEZ");
+			puts("3) DR. ALFREDO TALAVERA DIAZ");
+			puts("4) SALIR");
+			puts("ESCOGE UNA OPCION");
+			scanf("%d",&op);
+			system("cls");
+						switch(op)
+					{
+						case 1:
+							
+							
+							puts("\t\t BIENVENIDO, ESCOGIO A DR. ANDRES INIESTA LUJAN");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+							
+						
+						case 2:
+						
+							
+							
+							puts("\t\t BIENVENIDO, DR. JOSE DE JESUS CORONA RODRIGUEZ");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+							
+						
+						case 3:
+							
+							
+							
+							puts("\t\t BIENVENIDO, ESCOGIO A DR. ALFREDO TALAVERA DIAZ");
+							puts("QUE DESEA HACER");
+							puts("1) ALTAS");
+							puts("2) BAJAS");
+							puts("3) CONSULTA");
+							puts("4) SALIR");
+							puts("ESCOGA UNA OPCION");
+							scanf("%d",&op);
+							system("cls");
+								
+								switch(op)
+								{
+									
+									case 1:
+										puts("\t\t ALTAS");
+										break;	
+									
+									case 2:
+										puts("\t\t BAJAS");
+										break;
+									
+									case 3:
+										puts("\t\t CONSULTA");
+										break;
+								}
+							break;
+							
+						
+						
+							
+					}
+			
+			}while(op!=4);	
+			break;
+			
+			
+		break;
+		case 4:
+			puts("SALIR");
+			break;
+		
+		default :
+			puts("ZONA NO DISPONIBLE");
+			break;
+	}
+}while(op!=4);
+}
+
